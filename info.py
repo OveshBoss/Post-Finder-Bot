5e213cd49860f8b0ec011cd4490e9fa19b9a9b1b
ovesh boss <oveshboss.08@gmail.com>
2025-12-25 12:15:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
+API_ID       = int(environ.get("API_ID", "23903140"))
+API_HASH     = environ.get("API_HASH", "579f1bcf3eac1660d81ef34b09906012")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1003166629808"))
+ADMIN        = int(environ.get("ADMIN", "1416433622"))
+CHANNEL      = environ.get("CHANNEL", "@OveshBossOfficial")
